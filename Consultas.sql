8797489b222c3b0efdf455b6a86938c464a0536a
CreamySweets <victorfc_1804@hotmail.com>
2020-08-14 20:02:04 -0500

    Adición de consultas

--- Consultas.sql
+++ Consultas.sql
@@ -1,32 +1,47 @@
-SELECT DISTINCT cliente.telefono FROM cliente
+SELECT DISTINCT cliente.telefono AS "Teléfono de todos los clientes que compraron alcohol" FROM cliente
 JOIN venta ON venta.idCliente = cliente.RFC
 JOIN productosVenta ON productosVenta.idVenta = venta.idVenta
 JOIN productos ON productos.idProducto = productosVenta.idProducto
 WHERE productos.nombre LIKE 'Alcohol%';
 
-SELECT venta.idVenta FROM venta
+SELECT venta.idVenta AS "Números de venta mayores a $150 vendidas por Víctor" FROM venta
 JOIN empleado ON venta.idEmpleado = empleado.idEmpleado
 WHERE venta.montoTotal > 150.00 AND empleado.nombre LIKE 'Victor%';
 
-SELECT COUNT(productosVenta.idProducto) FROM productosVenta
+SELECT COUNT(productosVenta.idProducto) AS "Cantidad de productos vendidos con el código de barras '7501011105171'" FROM productosVenta
 JOIN productos ON productos.idProducto = productosVenta.idProducto
 WHERE productosVenta.idProducto = 7501011105171;
 
-SELECT DISTINCT empleado.nombre FROM empleado
+SELECT DISTINCT empleado.nombre AS "Nombre del empleado que ha vendido Jabón Zote Blanco de 400g" FROM empleado
 JOIN venta ON venta.idEmpleado = empleado.idEmpleado
 JOIN productosVenta on productosVenta.idVenta = venta.idVenta
 JOIN productos ON productos.idProducto = productosVenta.idProducto
 WHERE productos.nombre LIKE 'Jabón Zote Blanco 400g';
 
-SELECT DISTINCT productos.nombre FROM productos
+SELECT DISTINCT productos.nombre "Nombre de los productos mayores a 20 pesos vendidos por Víctor" FROM productos
 JOIN productosVenta ON productosVenta.idProducto = productos.idProducto
 JOIN venta ON venta.idVenta = productosVenta.idVenta
 JOIN empleado ON empleado.idEmpleado = venta.idEmpleado
 WHERE productos.precioVenta > 20.00 AND empleado.nombre LIKE 'Victor%';
 
-SELECT DISTINCT cliente.nombre FROM cliente
+SELECT DISTINCT cliente.nombre "Nombre de los clientes que han sido atendidos por Gilberto" FROM cliente
 JOIN venta ON venta.idCliente = cliente.RFC
 JOIN empleado ON empleado.idEmpleado = venta.idEmpleado
 WHERE empleado.nombre LIKE 'Gilberto%';
 
-SELECT productos.costoMayoreo
+SELECT productos.nombre AS "Nombre de todos los productos vendidos en la venta 6 con costo de mayoreo menor a $10.00", productos.costoMayoreo AS "Costo de mayoreo" FROM productos
+JOIN productosVenta ON productosVenta.idProducto = productos.idProducto
+JOIN venta ON venta.idVenta = productosVenta.idVenta
+WHERE venta.idVenta = 6 AND productos.costoMayoreo < 10.00;
+
+SELECT COUNT(productos.idProducto) AS "Cantidad total de Sun Chips compradas por el cliente con RFC 'CEHM0109S99'" FROM productos
+JOIN productosVenta ON productosVenta.idProducto = productos.idProducto
+JOIN venta ON venta.idVenta = productosVenta.idVenta
+JOIN cliente ON cliente.RFC = venta.idCliente
+WHERE cliente.RFC LIKE 'CEHM0109S99' AND productos.nombre LIKE 'Sun Chips%';
+
+SELECT venta.idVenta AS "Número de venta en las cuales se han vendido Triki-Trakes" FROM venta
+JOIN productosVenta ON productosVenta.idVenta = venta.idVenta
+JOIN productos ON productos.idProducto = productosVenta.idProducto
+WHERE productos.nombre LIKE '%Triki-Trakes%';
+
